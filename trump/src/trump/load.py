20142c630b877c1f89a218b5e0356d748abe0f63
Lia <liabifano@gmail.com>
2017-11-24 15:28:51 +0100

    half general analysis

--- trump/src/trump/load.py
+++ trump/src/trump/load.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 from functools import reduce
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 DATA_FOLDER = os.path.join(os.path.abspath(os.path.join(__file__, '../../../..')))
 YEARS = ['2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009']
@@ -28,13 +29,18 @@ def load_tweets():
     data = reduce(lambda x, y: x + y, data)
 
     df = pd.io.json.json_normalize(data)[COLS]
+    df['created_at'] = pd.to_datetime(df['created_at'])
 
     # Filter just Trump, there is around 300 tweets that is not from him
     df = df[df['user.id'] == 25073877]
 
-    df['created_at'] = pd.to_datetime(df['created_at'])
+    # Filter missing text - around 1200 all from 2017
+    df = df[pd.notnull(df['text'])]
+    df[df['created_at'] >= datetime(2017, 4, 30)]
+
     df['weekday'] = df['created_at'].dt.weekday_name
     df['month_cohort'] = df['created_at'].dt.strftime('%Y-%m')
+    df['first_day_month_cohort'] = pd.to_datetime(df['month_cohort'])
     df['quarter_cohort'] = pd.PeriodIndex(df['created_at'], freq='Q')
     df['is_weekend'] = df['weekday'].apply(lambda x: x in ['Saturday', 'Sunday'])
 
