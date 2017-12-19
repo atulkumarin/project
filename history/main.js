aa62df8ab0507a49cbde7d038f918b7a38f74257
Lia <liabifano@gmail.com>
2017-12-19 22:21:18 +0100

    word2vec

--- history/main.js
+++ history/main.js
@@ -1,7 +1,4 @@
-const word2vecFile = 'figures/word2vec.json';
-
-
 d3.json(word2vecFile, function (error, data) {
     console.log("hey");
-    console.log(data['speaking'])
+    console.log(filterWords(data, 'speaking'))
 });
